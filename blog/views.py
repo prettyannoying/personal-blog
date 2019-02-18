8ae2d949191b75670fda3c94a9209278f30893fc
skenypatel <skeny27@gmail.com>
2019-02-18 22:16:54 +0530

    modified home page

--- blog/views.py
+++ blog/views.py
@@ -14,14 +14,14 @@ def home(request):
 	context={
 		'posts':Post.objects.all()
 	}
-	return render(request, 'blog/home.html',context)
+	return render(request, 'blog/index.html',context)
 
 def about(request):
 	return render(request, 'blog/about.html')
 
 class PostListView(ListView):
 	model=Post
-	template_name='blog/home.html' #<app>/<model>_<viewtype>.html
+	template_name='blog/index.html' #<app>/<model>_<viewtype>.html
 	context_object_name='posts'
 	ordering= ['-date_posted']
 	paginate_by = 5
